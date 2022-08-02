a42ddfd49dda3253847250fbc7f29219d15285fb
Rangel Netto <rangelnetto@sympla.com.br>
2022-08-02 17:58:04 -0300

    feat: second part.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,68 @@
+import { HistoryList, HistoryContainer, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status color="green">Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 2</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status color="green">Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 3</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status color="green">Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 4</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status color="red">Interrompido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 5</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status color="yellow">Em andamento</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,55 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import {
   HomeContainer,
   FormContainer,
   CountDownContainer,
   Separator,
+  StartCountdownButton,
+  MinutesAmountInput,
+  TaskInput,
 } from './styles'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  const handleCreateNewCycle = (data: any) => {
+    console.log('submit data', data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" type="text" />
+          <TaskInput
+            id="task"
+            type="text"
+            list="task-suggestion"
+            placeholder="Dê um nome ao projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestion">
+            <option value="galo 1" />
+            <option value="galo 2" />
+            <option value="Limanha de ferro" />
+            <option value="Dancing" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input id="minutesAmount" type="text" />
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +62,10 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
